9daa822fa54636b28a2bc156f2cae2a501cd8dd1
JingWang <wangjing061993@gmail.com>
2023-04-25 16:33:00 +0200

    Aggiornato

--- app.js
+++ app.js
@@ -6,15 +6,17 @@ const circle = new Circle(499.5, 250, 10, 0, 2 * Math.PI);
 const circle1 = new Circle(499.5, 250, 10, 0, 2 * Math.PI);
 
 function whiteRectangles() {
-    ctx.fillStyle = '#fafafa';
-    ctx.fillRect(333-50, 0, 50,myCanvas.height)
-    ctx.fillRect(666-25, 0, 50,myCanvas.height)
-    ctx.fillStyle = 'rgba(15, 2, 109, 0.6)';
+   
+   
+    ctx.fillStyle = 'rgba(255, 255, 0, 1)';
     // ctx.fillRect(0,0,283,myCanvas.height); 
     // ctx.fillRect(333,0,308,myCanvas.height);
     // ctx.fillRect(691,0,308,myCanvas.height);
     circle.draw(ctx);
     circle.changePosition();  
+    ctx.fillStyle = '#fafafa';
+    ctx.fillRect(333-50, 0, 50,myCanvas.height)
+    ctx.fillRect(666-25, 0, 50,myCanvas.height)
     window.requestAnimationFrame(whiteRectangles);
 }
 window.requestAnimationFrame(whiteRectangles);
--- circle.js
+++ circle.js
@@ -3,40 +3,46 @@ class Circle{
         this.x = x;
         this.y = y;
         this.radius = radius;
-        this.xstartAngle = startAngle;
+        this.startAngle = startAngle;
         this.endAngle = endAngle;
+        this.speedx = (Math.random()-Math.random())*10
+        this.speedy = (Math.random()-Math.random())*10
     }
 
     draw(ctx) {
+      console.log(this);  
       ctx.beginPath();
-      ctx.fillStyle='rgba(200,220,180,0.9)';
-        // ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+    //   ctx.fillStyle='rgba(200,220,180,0.9)';
+        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
         // ctx.fillRect(this.x, this.y, 10, 10);
-        ctx.arc(499.5, 250, 10, 0, 2 * Math.PI);
+        // ctx.arc(499.5, 250, 10, 0, 2 * Math.PI);
         ctx.fill()
-        
-console.log(this.x);
-console.log(this.y);
+       
+
     }
     
     changePosition(){
-        this.x += (Math.random()-Math.random())*100
-        this.y += (Math.random()-Math.random())*100
+        this.x += this.speedx;
+        this.y += this.speedy;
+        // this.x += (Math.random()-Math.random())*10
+        // this.y += (Math.random()-Math.random())*10
         if (this.x<=0) {
-            this.x = 0;
+            // this.x = 0;
+            this.speedx*=-1
         }
         if (this.y<=0) {
-            this.y = 0;
+            // this.y = 0;
+            this.speedy*=-1
         }
         if (this.x>=myCanvas.width) {
-            this.x = myCanvas.width;
+            // this.x = myCanvas.width;
+            this.speedx*=-1
         }
         if (this.y>=myCanvas.height) {
-            this.y = myCanvas.height;
+            // this.y = myCanvas.height;
+            this.speedy*=-1
         }
 
     }
 }
 
-console.log(this.x);
-console.log(this.y);
